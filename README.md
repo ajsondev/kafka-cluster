cc764fdf8cb0393199b3c21f85365d21fce9fb52
sundy <sundyli@truxing.com>
2017-05-20 11:38:34 +0800

    inital

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # kafka-cluster
 docker kafka-cluster
+
+## docker-hub
+
+## usage
+	- dataDir: /data/db/
+
+## example
+```
+docker run --net host --name kafka \
+	-p 9200:9200 \
+	-v /data/db/kafka:/data/db/ \
+	-d sundyli/kafka-cluster  ${id} lp2,lp3,lp4 
+```
---
+++ entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+mkdir -p $dataLogDir
+
+export id=$1
+export host=$2
+export zks=$3
+
+##build kafka config
+kconfig="broker.id=${id}
+num.io.threads=8socket.send.buffer.bytes=102400
+socket.receive.buffer.bytes=102400
+socket.request.max.bytes=104857600
+num.partitions=1
+num.recovery.threads.per.data.dir=1
+log.retention.hours=12
+log.segment.bytes=1073741824
+log.retention.check.interval.ms=300000
+zookeeper.connection.timeout.ms=6000
+"
+kconfig="${kconfig}"$'\n'"advertised.listeners=PLAINTEXT://${host}:9092"
+kconfig="${kconfig}"$'\n'"log.dirs=${dataLogDir}"
+kconfig="${kconfig}"$'\n'"zookeeper.connect=${zks}"
+
+echo "${kconfig}" > config/server.properties
+
+##start the kafka server
+bin/kafka-server-start.sh config/server.properties
